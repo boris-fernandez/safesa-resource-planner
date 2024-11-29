7b1727e9aba5481e6efcb5b4c95d6ef19b0b58a2
Boris <164273833+borisfernande@users.noreply.github.com>
2024-11-29 15:19:00 -0500

    Mensaje de commit que describe los cambios

--- src/com/safesa/app/main/Main.java
+++ src/com/safesa/app/main/Main.java
@@ -3,10 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.safesa.app.main;
-
-import com.safesa.app.dto.ClienteDto;
-import com.safesa.app.model.Cliente;
-
 /**
  *
  * @author BORIS
@@ -18,10 +14,6 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //int clienteID, String dni, String nombre, String apellidos, String telefono, String email
-        var CLINETE1 = new Cliente("759841036","Boris", "Fernandez Cabrera", "974035479","borisfernandezcabrera@gmail.com");
-        var cliente = new ClienteDto();
-        cliente.agregarCliente(CLINETE1);
     }
     
 }
